14cf4c102a6c212ac50fe73ef81345cc6beea6df
guptapratykshh <pratykshgupta9999@gmail.com>
2026-04-05 17:26:14 +0530

    Revert "Fix stdexec future dataflow dispatch"
    This reverts commit c83517e373e9badf076133a6dfddd448e1d4b468.
    
    Signed-off-by: guptapratykshh <pratykshgupta9999@gmail.com>

--- libs/core/algorithms/include/hpx/parallel/algorithms/partial_sort.hpp
+++ libs/core/algorithms/include/hpx/parallel/algorithms/partial_sort.hpp
@@ -401,7 +401,6 @@ namespace hpx::parallel {
                 }
 
                 return hpx::dataflow(
-                    hpx::launch::async,
                     [last](hpx::future<Iter>&& leftf,
                         hpx::future<Iter>&& rightf) -> Iter {
                         if (leftf.has_exception() || rightf.has_exception())
